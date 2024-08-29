6ddea3c49415e7581e36a02dd5f44ed8fecdc444
muxueqz <zhangmingyuan240@gmail.com>
2024-08-29 11:52:27 +0800

    use dunst as the notification service

--- home/muxueqz/.config/i3/autostart.sh
+++ home/muxueqz/.config/i3/autostart.sh
@@ -8,6 +8,6 @@ PATH=$PATH:$PWD CM_SELECTIONS='clipboard' ./clipmenud &
 cd -
 
 /home/muxueqz/workspaces/x-apps-in-container/src/app_server &> /dev/shm/x-apps-in-container.log &
-/usr/lib/notification-daemon/notification-daemon &
+dunst &
 
 /usr/bin/gnome-keyring-daemon --components=pkcs11,secrets --control-directory=/run/user/1000/keyring
