a43d410eab01b03ad237f7f5a377e1b0d9e7b575
muxueqz <zhangmingyuan240@gmail.com>
2024-09-11 18:58:47 +0800

    remove unuse code

--- home/muxueqz/.local/bin/start-sway.sh
+++ home/muxueqz/.local/bin/start-sway.sh
@@ -1,9 +1,2 @@
-#dbus-launch --sh-syntax --exit-with-session /data/work/project/qtile-env/bin/qtile &>> /tmp/qtile.log
-# dbus-launch --sh-syntax --exit-with-session /data/work/project/qtile-env/bin/qtile
-
-# dbus-launch --sh-syntax --exit-with-session $HOME/qtile-env/bin/qtile start
-ln -sfv ~/.config/sway/config-$HOSTNAME /run/user/1000/sway-config-device
-dbus-launch --sh-syntax --exit-with-session sway
-
-#dbus-launch --sh-syntax --exit-with-session /usr/bin/st
-#dbus-launch --sh-syntax --exit-with-x11 /data/work/project/qtile-env/bin/qtile >> /tmp/qtile.log
+#!/bin/sh
+dbus-launch --sh-syntax --exit-with-session sway -c ~/.config/sway/config-$HOSTNAME
