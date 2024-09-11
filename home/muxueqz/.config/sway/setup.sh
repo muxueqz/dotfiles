e06985dcfb60c7333660a57524cad7f3fbf0031c
muxueqz <zhangmingyuan240@gmail.com>
2024-09-11 18:57:23 +0800

    simplify

--- home/muxueqz/.config/sway/setup.sh
+++ home/muxueqz/.config/sway/setup.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-rsync -av --progress ./swayidle.sh ./swaylock-with-idle.sh /opt/libexec/
-sudo chown root:root /opt/libexec/swayidle.sh /opt/libexec/swaylock-with-idle.sh
-sudo chmod 755 /opt/libexec/swayidle.sh /opt/libexec/swaylock-with-idle.sh
+sudo install -v -o root -g root -m 755 ./swayidle.sh /opt/libexec/
