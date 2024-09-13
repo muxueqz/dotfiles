435ced5510795ab899516a8ef9ea4f9e3629d627
muxueqz <zhangmingyuan240@gmail.com>
2024-09-13 23:11:42 +0800

    use wl-clipboard-history replace clipmenu

--- home/muxueqz/.config/sway/autostart.sh
+++ home/muxueqz/.config/sway/autostart.sh
@@ -5,8 +5,8 @@
 fcitx5 -d
 mako &
 
-cd /data/work/projects/clipmenu
-./clipmenud &
+cd /data/work/projects/wl-clipboard-history/
+./wl-clipboard-history -t &
 
 redshift -l 26.0960622048:119.3065881729 &
 gnome-keyring-daemon --start --components=secrets
