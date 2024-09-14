96f21dd4a6c65302d6bfe614d65ad61c30818022
muxueqz <zhangmingyuan240@gmail.com>
2024-09-14 12:16:10 +0800

    otp auto type

--- home/muxueqz/.local/bin/nobleprog-get-otp.sh
+++ home/muxueqz/.local/bin/nobleprog-get-otp.sh
@@ -3,4 +3,5 @@ OTP=$(i3-msg -t get_tree |
 	jello | grep '| NobleProg User' | grep -E "title|name" | cut -d'|' -f1 | cut -d'"' -f4 | grep -o '\S*'
   )
 echo $OTP
-echo $OTP | toclip
+# echo $OTP | toclip
+echo $OTP | wtype -
