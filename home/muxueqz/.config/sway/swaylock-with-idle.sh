f870075260c7b6f4530bf930080d3517c031d237
muxueqz <zhangmingyuan240@gmail.com>
2024-12-23 17:29:44 +0800

    if time is between 22:00 to 09:00, timeout is 10 seconds

--- home/muxueqz/.config/sway/swaylock-with-idle.sh
+++ home/muxueqz/.config/sway/swaylock-with-idle.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 touch /tmp/last_input_time
+# if time is between 22:00 to 09:00, timeout is 10s
+timeout=3600
+if test $(date +%H) -gt 22 -o $(date +%H) -lt 9; then
+  timeout=10
+fi
 sudo /opt/libexec/swayidle.sh &
 swaylock $@
 
