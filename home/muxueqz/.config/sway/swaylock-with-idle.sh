1eb35c60b31d4a4f1de3dc49b1f11e97f376b308
muxueqz <zhangmingyuan240@gmail.com>
2025-01-08 21:48:24 +0800

    if error then exit

--- home/muxueqz/.config/sway/swaylock-with-idle.sh
+++ home/muxueqz/.config/sway/swaylock-with-idle.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+set -e
 touch /tmp/last_input_time
 # if time is between 22:00 to 09:00, timeout is 10s
 timeout=3600
 if test $(date +%H) -gt 22 -o $(date +%H) -lt 9; then
   timeout=10
 fi
-sudo /opt/libexec/swayidle.sh &
+sudo /opt/libexec/swayidle.sh $timeout &
 swaylock $@
 
 for i in $(seq 10); do
