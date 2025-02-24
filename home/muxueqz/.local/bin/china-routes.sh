eeb25fef94f9898065a2752f76ccde5e432dab0a
muxueqz <zhangmingyuan240@gmail.com>
2025-02-24 15:59:48 +0800

    quiet output

--- home/muxueqz/.local/bin/china-routes.sh
+++ home/muxueqz/.local/bin/china-routes.sh
@@ -2,6 +2,6 @@
 CHINA_GW=192.168.101.254
 
 # for i in $(cat china_ip_list.txt); do sudo ip route add $i via $CHINA_GW; done
-for i in $(wget -O - 'https://github.com/17mon/china_ip_list/raw/refs/heads/master/china_ip_list.txt'); do
+for i in $(wget -q -O - 'https://github.com/17mon/china_ip_list/raw/refs/heads/master/china_ip_list.txt'); do
 	sudo ip route add $i via $CHINA_GW
 done
