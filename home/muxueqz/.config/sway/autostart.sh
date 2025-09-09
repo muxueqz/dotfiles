306f4719c25ba7aba84ed6eed2a5dc11f50fcb9a
muxueqz <zhangmingyuan240@gmail.com>
2025-09-09 12:51:01 +0800

    disable rclone mount

--- home/muxueqz/.config/sway/autostart.sh
+++ home/muxueqz/.config/sway/autostart.sh
@@ -10,3 +10,9 @@ cd /data/work/projects/wl-clipboard-history/
 
 redshift -l 26.0960622048:119.3065881729 &
 gnome-keyring-daemon --start --components=secrets
+
+# mkdir -pv /dev/shm/temp-workspaces/;
+# firejail --noprofile \
+#   --whitelist=/home/muxueqz/.local/bin/rclone \
+#   --whitelist=/home/muxueqz/.config/rclone/ \
+  rclone mount book: /dev/shm/temp-workspaces/boxbook/ &> /dev/shm/boxbook-mount.log &
