af52996ed4dccedb22007367e778002dacf1005f
muxueqz <zhangmingyuan240@gmail.com>
2025-09-10 13:24:46 +0800

    use fuzzel to list windows

--- home/muxueqz/.config/sway/list-window.sh
+++ home/muxueqz/.config/sway/list-window.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-# ilia -p textlist | (
-python3 $HOME/.config/sway/i3_tools.py select-window $1 |
-	rofi -p "Window" -dmenu -i | (
-	read -r num id name
-	swaymsg "[con_id=$id]" focus
-)
+python3 $HOME/.config/sway/i3_tools.py select-window $1
